7bf68ccea096695577b215d6a7cf8439c3dfe139
Kim Yongwook <kim.yongwook.snu@gmail.com>
2021-02-18 15:07:11 +0900

    limited rotation and movement with according axis

--- Assets/Scripts/XRRestrictedMovement.cs
+++ Assets/Scripts/XRRestrictedMovement.cs
@@ -3,8 +3,9 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.XR.Interaction.Toolkit;
 
-
-public class XRRotationOnly : XRBaseInteractable
+[DisallowMultipleComponent]
+[RequireComponent(typeof(Rigidbody))]
+public class XRRestrictedMovement : XRBaseInteractable
 {
 
     // variables from offset grabbable
@@ -22,10 +23,12 @@ public class XRRotationOnly : XRBaseInteractable
 
     // variable from dial interactable
     XRBaseInteractor m_GrabbingInteractor;
-    Quaternion m_GrabbedRotation;
 
     // rotation axis is x axis (1, 0, 0)
 
+    public bool allowRotation = true;
+    public bool allowTranslateY = true;
+    public bool allowTranslateZ = true;
 
 
     protected override void Awake()
@@ -42,13 +45,39 @@ public class XRRotationOnly : XRBaseInteractable
         {
             if (updatePhase == XRInteractionUpdateOrder.UpdatePhase.Fixed)
             {
-                m_Rb.angularVelocity = Vector3.zero;
-                Quaternion grabRotation = m_Rb.transform.rotation;
-                Quaternion difference = m_GrabbingInteractor.attachTransform.rotation * Quaternion.Inverse(grabRotation);
-                
-                var rotationX = Quaternion.AngleAxis(difference.eulerAngles.x, Vector3.right);
-                
-                m_Rb.MoveRotation(grabRotation * rotationX);
+                if (allowTranslateY || allowTranslateZ)
+                {
+                    // Limit desired movement within specified length?
+
+                    // get center of mass relative to transform center
+                    Vector3 rigidBodyPosition = m_Rb.worldCenterOfMass;
+                    // change world location of grabber relative to this transform and calc difference
+                    // difference of point == direction
+                    Vector3 difference = transform.InverseTransformDirection(m_GrabbingInteractor.attachTransform.position - rigidBodyPosition);
+                    // Find the local position difference in local location
+                    var diffy = allowTranslateY ? difference.y : 0f;
+                    var diffz = allowTranslateZ ? difference.z : 0f;
+                    // calculate final position delta and translate back to world location
+                    var positionDelta = new Vector3(0, diffy, diffz);
+                    var worldPositionDelta = transform.TransformDirection(positionDelta);
+                    // apply location difference
+                    m_Rb.velocity = Vector3.zero;
+                    m_Rb.MovePosition(m_Rb.position + worldPositionDelta);
+                }
+
+                if (allowRotation)
+                {
+                    // check world rotaion or local rotation(is desired)
+                    m_Rb.angularVelocity = Vector3.zero;
+                    // find rotation and find difference
+                    Quaternion rigidBodyRotation = m_Rb.transform.rotation;
+                    Quaternion difference = m_GrabbingInteractor.attachTransform.rotation * Quaternion.Inverse(rigidBodyRotation);
+
+                    var rotationX = Quaternion.AngleAxis(difference.eulerAngles.x, Vector3.right);
+
+                    m_Rb.MoveRotation(rigidBodyRotation * rotationX);
+                }
+
             }
         }
     }
@@ -57,6 +86,7 @@ public class XRRotationOnly : XRBaseInteractable
     {
         if (interactor is XRDirectInteractor)
         {
+            
             SavedTransform savedTransform = new SavedTransform();
 
             savedTransform.OriginalPosition = interactor.attachTransform.localPosition;
@@ -73,8 +103,9 @@ public class XRRotationOnly : XRBaseInteractable
                 m_OriginalSceneParent = transform.parent;
                 transform.parent = null;
             }
+            // save grabbing interactor for future use(i.e. calculating angle)
             m_GrabbingInteractor = interactor;
-            m_GrabbedRotation = m_GrabbingInteractor.transform.rotation.normalized;
+            // m_GrabbedRotation = m_GrabbingInteractor.transform.rotation.normalized;
 
         }
 
@@ -95,9 +126,14 @@ public class XRRotationOnly : XRBaseInteractable
             }
         }
 
+        // Retain original transform parent
         transform.parent = m_OriginalSceneParent;
 
+        // Set grabbing interactor to null again
         m_GrabbingInteractor = null;
+
+        // Snap position and rotation to nearest 0.05 and 15 degree
+
 
         base.OnSelectExit(interactor);
     }
