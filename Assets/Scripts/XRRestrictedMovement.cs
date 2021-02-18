3aa7121d20ead4e9859bbf863487dbdfe3bbbdea
Kim Yongwook <kim.yongwook.snu@gmail.com>
2021-02-18 15:54:59 +0900

    Limit translation according to original parent within specific degree

--- Assets/Scripts/XRRestrictedMovement.cs
+++ Assets/Scripts/XRRestrictedMovement.cs
@@ -8,6 +8,8 @@ using UnityEngine.XR.Interaction.Toolkit;
 public class XRRestrictedMovement : XRBaseInteractable
 {
 
+    const float k_Step = 0.05f;
+
     // variables from offset grabbable
     class SavedTransform
     {
@@ -30,6 +32,14 @@ public class XRRestrictedMovement : XRBaseInteractable
     public bool allowTranslateY = true;
     public bool allowTranslateZ = true;
 
+    // limit range +- translationRange * 0.05f + -0.025f(for margin)
+    // if 3 ==> -0.175 ~ 0.175(will snap to 0.15)
+    [Range(1, 5)]
+    public int translationRange = 3;
+
+    float m_TranslationRangeMin => -1 * (translationRange * k_Step + k_Step / 2);
+    float m_TranslationRangeMax => m_TranslationRangeMin * -1;
+    
 
     protected override void Awake()
     {
@@ -41,32 +51,42 @@ public class XRRestrictedMovement : XRBaseInteractable
 
     public override void ProcessInteractable(XRInteractionUpdateOrder.UpdatePhase updatePhase)
     {
+        // In this step, object has no parent. if must refer original parent, use m_OriginalSceneParent
         if (isSelected)
         {
             if (updatePhase == XRInteractionUpdateOrder.UpdatePhase.Fixed)
             {
                 if (allowTranslateY || allowTranslateZ)
                 {
-                    // Limit desired movement within specified length?
-
+                    m_Rb.velocity = Vector3.zero;
                     // get center of mass relative to transform center
                     Vector3 rigidBodyPosition = m_Rb.worldCenterOfMass;
                     // change world location of grabber relative to this transform and calc difference
                     // difference of point == direction
-                    Vector3 difference = transform.InverseTransformDirection(m_GrabbingInteractor.attachTransform.position - rigidBodyPosition);
+                    Vector3 difference = m_OriginalSceneParent.transform.InverseTransformDirection(m_GrabbingInteractor.attachTransform.position - rigidBodyPosition);
                     // Find the local position difference in local location
                     var diffy = allowTranslateY ? difference.y : 0f;
                     var diffz = allowTranslateZ ? difference.z : 0f;
-                    // calculate final position delta and translate back to world location
+                    // calculate final position delta and check against original parent
                     var positionDelta = new Vector3(0, diffy, diffz);
-                    var worldPositionDelta = transform.TransformDirection(positionDelta);
+                    var finalPosition = m_OriginalSceneParent.transform.InverseTransformPoint(m_Rb.position) + positionDelta;
+
+                    // Limit desired movement within specified length
+                    finalPosition.y = Mathf.Clamp(finalPosition.y, m_TranslationRangeMin, m_TranslationRangeMax);
+                    finalPosition.z = Mathf.Clamp(finalPosition.z, m_TranslationRangeMin, m_TranslationRangeMax);
+
+                    //var worldPositionDelta = transform.TransformDirection(positionDelta);
                     // apply location difference
-                    m_Rb.velocity = Vector3.zero;
-                    m_Rb.MovePosition(m_Rb.position + worldPositionDelta);
+                    
+                    // change back to world position
+                    var finalWorldPosition = m_OriginalSceneParent.transform.TransformPoint(finalPosition);
+
+                    m_Rb.MovePosition(finalWorldPosition);
                 }
 
                 if (allowRotation)
                 {
+                    // FIXME: make rotate relative to original parent, not world or hand.
                     // check world rotaion or local rotation(is desired)
                     m_Rb.angularVelocity = Vector3.zero;
                     // find rotation and find difference
